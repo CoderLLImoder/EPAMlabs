b2569b73e34431605a61662139e046f4397c6338
Илья Капёрский <kaperskiyi@gmail.com>
2016-03-17 19:09:26 +0400

    go

---
+++ Капёрский_Илья_Task07/Task2/program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Office jhon = new Office("Джон");
+            Office hugo = new Office("Хьюго");
+            Office bill = new Office("Билл");
+
+            Office.CameToWork(jhon);
+            Office.CameToWork(hugo);
+            Office.CameToWork(bill);
+            Office.GoHome(jhon);
+            Office.GoHome(hugo);
+            Office.GoHome(bill);
+
+            
+            Console.ReadKey();
+        }
+
+        delegate void Message(string name);
+    }
+}
