adad4847be6b7a30f286d86bd098a5824414faf9
Илья Капёрский <kaperskiyi@gmail.com>
2016-03-17 23:21:03 +0400

    go

---
+++ Капёрский_Илья_Task08/Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class DataHelper
+    {
+        public static int Sum(this int[] a)
+        {
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            return sum;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 10;
+            int[] a = new int[n];
+            Random rnd = new Random();
+            Console.Write("Исходный массив:");
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = rnd.Next(-100, 100);
+                Console.Write(" " + a[i]);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Сумма элементов массива = {0}", a.Sum());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Капёрский_Илья_Task08/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public static class DataHelper
+    {
+        /// <summary>
+        /// Расширяющий метод, проверяет строку на положительное число.
+        /// </summary>
+
+        public static bool StringToInt(this string s)
+        {
+            bool c = true;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s == "") { Console.WriteLine("Строка является пустой"); }
+                else
+                {
+                    if ((s[i] != '0') && (s[i] != '1') && (s[i] != '2') && (s[i] != '3') && (s[i] != '4') && (s[i] != '5') && (s[i] != '6') && (s[i] != '7') && (s[i] != '8') && (s[i] != '9'))
+                        c = false;
+                }
+
+            }
+            return c;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Введите строку:");
+                string str = Console.ReadLine();
+                bool Z = str.StringToInt();
+                if (Z) Console.WriteLine("Строка является положительным целым числом.");
+                else Console.WriteLine("Строка не является положительным целым числом.");
+                Console.ReadKey();
+         }
+    }
+}
