30c4208b6956879ef2016020db582d5afee2ab96
Илья Капёрский <kaperskiyi@gmail.com>
2016-03-18 21:29:53 +0400

    go

---
+++ Капёрский_Илья_Task09/Task1/FileWatcher.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Task1
+{
+    public class FileWatcher
+    {
+        public static string Path = "C:\\Новая папка";
+        public static string Temp = @"temp"; //папка создается в дебаге
+        public static string[] AllDates = new string[100];
+        public static int i = 0;
+
+
+        public static DateTime ReadDate()
+        {
+            Console.WriteLine(">>>Введите дату в формате дд.мм.гггг чч:мм:сс");
+            DateTime time = Convert.ToDateTime(Console.ReadLine());
+            return time;
+        }
+
+        
+
+        //метод Clone(DirectoryInfo savingfolder, DirectoryInfo tempfolder) клонирует savingfolder в tempfolder
+        public static void Clone(DirectoryInfo savingfolder, DirectoryInfo tempfolder)
+        {
+            foreach (FileInfo file in savingfolder.EnumerateFiles())
+            {
+                FileInfo newfile = new FileInfo(tempfolder.FullName + '\\' + file.Name);
+                using (newfile.Create()) { }
+                file.CopyTo(newfile.FullName, true);
+            }
+
+            foreach (DirectoryInfo dir in savingfolder.EnumerateDirectories())
+            {
+                DirectoryInfo newdir = tempfolder.CreateSubdirectory(dir.Name);
+                Clone(dir, newdir);
+            }
+        }
+
+        //событие изменения файла
+        public static void onChange(object source, FileSystemEventArgs e)
+        {
+            //сообщаем об изменении
+            Console.WriteLine("Файл '{0}' был изменён!", e.FullPath);
+
+            //сохраняем изменения в папку с именем текущей даты
+            char[] c = DateTime.Now.ToString().ToCharArray();
+            while (Array.IndexOf(c, ':') != -1)
+                c[Array.IndexOf(c, ':')] = '-';
+            string date = new string(c);
+            DirectoryInfo tempfolder = new DirectoryInfo(Temp + date);
+            tempfolder.Create();
+            Clone(new DirectoryInfo(Path), tempfolder);
+            AllDates[i] = date;
+            i++;
+            
+
+        }
+
+        public void Watching()
+        {
+            Console.WriteLine("\nРежим наблюдения включён.");
+            FileSystemWatcher watcher = new FileSystemWatcher(Path, "*.txt");
+            watcher.IncludeSubdirectories = true;
+            //подписываемся на событие
+            watcher.Changed += new FileSystemEventHandler(onChange);
+            //начинам слежение
+            watcher.EnableRaisingEvents = true;
+            Console.WriteLine(">>>Нажмите \'0\' чтобы завершить наблюдение.");
+            while (Console.Read() != '0') ;
+            watcher.EnableRaisingEvents = false;
+
+        }
+         
+        public void Return()
+        {
+            
+            Console.WriteLine("Даты изменений");
+            for (int j=0; j < i; j++)
+            {
+                Console.WriteLine(AllDates[j]);
+
+            }
+
+            //считываем дату
+            DateTime date = ReadDate();
+            char[] cdate = date.ToString().ToCharArray();
+            while (Array.IndexOf(cdate, ':') != -1)
+                cdate[Array.IndexOf(cdate, ':')] = '-';
+            //откатываем
+            Clone(new DirectoryInfo(Temp + new string(cdate)), new DirectoryInfo(Path));
+            Console.WriteLine("Произведён откат на {0}", date.ToString());
+        }
+    }   
+}
---
+++ Капёрский_Илья_Task09/Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            FileWatcher Watcher = new FileWatcher();
+            
+
+        int i = 0;
+            do
+            {
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Выберите режим:");
+                Console.WriteLine("1) Включить наблюдение.");
+                Console.WriteLine("2) Выполнить откат.");
+                Console.WriteLine("0) Выход из программы.");
+                Console.WriteLine("-------------------------");
+                char c = Console.ReadKey().KeyChar;
+                switch (c)
+                {
+                    case '1':
+                        Console.WriteLine(">>>Включен режим наблюдения.");
+                        Watcher.Watching();
+                        break;
+                    case '2':
+                        Console.WriteLine(">>>Включен режим отката изменений.");
+                        
+                        Watcher.Return();
+                        break;
+                    case '0':
+                        i++;
+                        break;
+                    default:
+                        Console.WriteLine(">>>Ошибка: неверное действие");
+                        break;
+                }
+            } while (i == 0);
+        }
+    }
+}
